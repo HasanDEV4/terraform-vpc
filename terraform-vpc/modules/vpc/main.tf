e5d6a89fe812a7b64f6d1ca8482136f65e0e4d4a
Hassan Ali Jauhar <91012251+HasanDEV4@users.noreply.github.com>
2025-01-20 17:17:16 +0500

    Update main.tf

--- terraform-vpc/modules/vpc/main.tf
+++ terraform-vpc/modules/vpc/main.tf
@@ -8,7 +8,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public" {
   count                   = length(var.azs)
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = cidrsubnet(var.vpc_cidr_block, 8, count.index)
+  cidr_block              = cidrsubnet(var.cidr_block, 8, count.index)
   availability_zone       = element(var.azs, count.index)
   map_public_ip_on_launch = true
 
