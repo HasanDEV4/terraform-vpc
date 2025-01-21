658687de2d56db053a9b865cd62b0efb68b1e035
Hassan Ali Jauhar <91012251+HasanDEV4@users.noreply.github.com>
2025-01-21 14:19:02 +0500

    Update main.tf

--- terraform-vpc/modules/ecs/main.tf
+++ terraform-vpc/modules/ecs/main.tf
@@ -1,26 +1,6 @@
-resource "aws_iam_role" "ecs_task_execution" {
-  name = "${var.container_name}-ecs-task-execution-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-
-  tags = {
-    Name = "${var.container_name}-ecs-task-execution-role"
-  }
-}
-
 resource "aws_iam_role_policy_attachment" "ecs_task_execution" {
   role       = aws_iam_role.ecs_task_execution.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  policy_arn = "arn:aws:iam::851725275673:role/ecsTaskExecutionRole"
 }
 
 resource "aws_ecs_cluster" "nginx" {
