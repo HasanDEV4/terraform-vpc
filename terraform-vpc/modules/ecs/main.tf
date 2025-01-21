e4a4494cd0eaa0809c60d199f45ecd6935893b1e
Hassan Ali Jauhar <91012251+HasanDEV4@users.noreply.github.com>
2025-01-21 14:33:43 +0500

    Update main.tf

--- terraform-vpc/modules/ecs/main.tf
+++ terraform-vpc/modules/ecs/main.tf
@@ -13,7 +13,7 @@ resource "aws_ecs_task_definition" "nginx" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = var.task_cpu
   memory                   = var.task_memory
-  execution_role_arn       = aws_iam_role.ecs_task_execution.arn
+  execution_role_arn       = "arn:aws:iam::851725275673:role/ecsTaskExecutionRole"
 
   container_definitions = jsonencode([
     {
