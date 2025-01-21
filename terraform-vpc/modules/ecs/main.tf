cecf1d87af5bd92107ef1c58334589bb37801eef
Hassan Ali Jauhar <91012251+HasanDEV4@users.noreply.github.com>
2025-01-21 17:42:49 +0500

    Update main.tf

--- terraform-vpc/modules/ecs/main.tf
+++ terraform-vpc/modules/ecs/main.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role_policy_attachment" "ecs_task_execution" {
-  role       = aws_iam_role.ecs_task_execution.name
+  role       = var.ecs_task_execution_role_name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
