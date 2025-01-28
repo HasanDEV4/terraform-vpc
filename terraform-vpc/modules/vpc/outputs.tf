c231226c040bf1f1ce7fc03fcf35dea90b13aa84
Hassan Ali Jauhar <91012251+HasanDEV4@users.noreply.github.com>
2025-01-28 18:25:30 +0500

    Update outputs.tf

--- terraform-vpc/modules/vpc/outputs.tf
+++ terraform-vpc/modules/vpc/outputs.tf
@@ -1,7 +1,3 @@
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
-
 output "public_subnet_ids" {
   value = aws_subnet.public[*].id
 }
@@ -10,11 +6,18 @@ output "private_subnet_ids" {
   value = aws_subnet.private[*].id
 }
 
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
 output "nat_gateway_id" {
-  value = aws_nat_gateway.main.id
+  value = aws_nat_gateway.nat_gateway[0].id
+  description = "The ID of the NAT Gateway."
 }
 
 output "private_route_table_id" {
-  value = aws_route_table.private.id
+  value = aws_route_table.private_route_table.id
+  description = "The ID of the private route table."
 }
+
 
