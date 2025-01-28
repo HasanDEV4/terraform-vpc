b1222be09517268d2f9dcebc4e76c144a5ed4da1
Hassan Ali Jauhar <91012251+HasanDEV4@users.noreply.github.com>
2025-01-28 17:45:39 +0500

    Update main.tf

--- terraform-vpc/modules/vpc/main.tf
+++ terraform-vpc/modules/vpc/main.tf
@@ -1,3 +1,4 @@
+# VPC
 resource "aws_vpc" "main" {
   cidr_block = var.cidr_block
   tags = {
@@ -5,6 +6,7 @@ resource "aws_vpc" "main" {
   }
 }
 
+# Internet Gateway
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
 }
@@ -36,27 +38,62 @@ resource "aws_subnet" "private" {
   }
 }
 
-# Route Table for Private Subnets
-resource "aws_route_table" "private_route_table" {
+# Route Table for Public Subnets
+resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.main.id
 
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
   tags = {
-    Name = "Private Route Table"
+    Name = "Public Route Table"
   }
 }
 
-# NAT Gateway (Agar `enable_nat_gateway = true` ho)
+# Associate Public Subnets with Public Route Table
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnet_cidrs)
+
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+# NAT Gateway (Enable only if var.enable_nat_gateway = true)
 resource "aws_eip" "nat" {
   count = var.enable_nat_gateway ? 1 : 0
+  vpc   = true
 }
 
 resource "aws_nat_gateway" "nat_gateway" {
-  count = var.enable_nat_gateway ? 1 : 0
-
+  count       = var.enable_nat_gateway ? 1 : 0
   allocation_id = aws_eip.nat[0].id
   subnet_id     = aws_subnet.public[0].id
 
   tags = {
     Name = "NAT Gateway"
   }
+}
+
+# Route Table for Private Subnets
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway[0].id
+  }
+
+  tags = {
+    Name = "Private Route Table"
+  }
+}
+
+# Associate Private Subnets with Private Route Table
+resource "aws_route_table_association" "private" {
+  count = length(var.private_subnet_cidrs)
+
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private_route_table.id
 }
